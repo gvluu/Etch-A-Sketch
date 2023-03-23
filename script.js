10d2f70f63d922431c9d35ad46a457d96ed2c804
gvluu <121136183+gvluu@users.noreply.github.com>
2023-03-23 16:00:26 -0700

    Add notes for code lines

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-window.onload = function() {
-    generateGrid();
+window.onload = function() { // event that triggers initiation AFTER page has finished loading.
+    initiateGrid(); // Ensures that function called only once the page is finished loading.
 };
 
-function generateGrid() {
-    const container = document.querySelector(".container");
+function initiateGrid() {
+    const container = document.querySelector(".container"); // pull from HTML
 
-    for (let i = 0; i < 16; i++) {
-        const row = document.createElement("div");
+    for (let i = 0; i < 16; i++) { // to create 16 rows
+        const row = document.createElement("div"); // instead of in HTML, create new element in DOM
         row.className = "row";
 
-        for (let i = 0; i < 16; i++) {
+        for (let i = 0; i < 16; i++) { // to create 16 squares in each row
             const square = document.createElement("div");
             square.className = "grid-square";
-            square.addEventListener("mouseover", function() {
-                square.classList.add("touch");
+            square.addEventListener("mouseover", function() { // 'mouseover' allows action to be heard for entire grid function
+                square.classList.add("touch"); // 'touch' keeps the color as a 'trail' for CSS at each square. gets added to HTML after hover.
             });
-            row.appendChild(square);
+            row.appendChild(square); // attach 'square' (aka 'grid-square') class to 'row' class
         }
 
-        container.appendChild(row);
+        container.appendChild(row); // attach 'row' class to 'container' class
     }
 }
