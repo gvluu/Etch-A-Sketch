d187dbe815ceb3a296c118608001bd90764a56f8
gvluu <121136183+gvluu@users.noreply.github.com>
2023-03-23 22:30:17 -0700

    Add code like evenListener so `reset` button works

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 window.onload = function() { // event that triggers initiation AFTER page has finished loading.
-    initiateGrid(); // Ensures that function called only once the page is finished loading.
+    generateGrid(); // Ensures that function called only once the page is finished loading.
 };
 
-function initiateGrid() {
-    const container = document.querySelector(".container"); // pull from HTML
+document.addEventListener("DOMContentLoaded", function() { // Ensures that JS code only runs after HTML is finished loading
+    const container = document.querySelector("#grid-container"); // was under function generateGrid but moved here for reason above
+    const resetButton = document.querySelector("#reset-btn");
 
+function generateGrid() {
     for (let i = 0; i < 16; i++) { // to create 16 rows
         const row = document.createElement("div"); // instead of in HTML, create new element in DOM
         row.className = "row";
@@ -13,11 +15,22 @@ function initiateGrid() {
             const square = document.createElement("div");
             square.className = "grid-square";
             square.addEventListener("mouseover", function() { // 'mouseover' allows action to be heard for entire grid function
-                square.classList.add("touch"); // 'touch' keeps the color as a 'trail' for CSS at each square. gets added to HTML after hover.
+                square.classList.add("touch"); // 'touch' keeps the color as a 'trail' for CSS. gets added to HTML after hover.
             });
-            row.appendChild(square); // attach 'square' (aka 'grid-square') class to 'row' class
+            row.appendChild(square); // attach 'square' class to 'row' class
         }
-
         container.appendChild(row); // attach 'row' class to 'container' class
     }
 }
+
+
+function resetGrid() { //need in order to remove touch class for reset
+    const squares = document.querySelectorAll(".grid-square");
+    squares.forEach(function(square) {
+      square.classList.remove("touch");
+    });
+  }
+
+  resetButton.addEventListener("click", resetGrid);
+  generateGrid();
+});
